09ddd5fafa3cb1a4cd1cec611aaf114eebc32d18
Brandon High <highb@users.noreply.github.com>
2023-02-27 20:44:20 -0800

    wtf is in this file?

--- download-and-verify.sh
+++ download-and-verify.sh
@@ -10,6 +10,7 @@ if [ -f "${KUSTOMIZE_FILENAME}" ]; then
 else
     curl -o "${KUSTOMIZE_FILENAME}" -L "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${VERSION}/${KUSTOMIZE_FILENAME}"
 fi
+cat "${KUSTOMIZE_FILENAME}"
 sha256sum "${KUSTOMIZE_FILENAME}"
 echo "${SHA256_CHECKSUM}  ${KUSTOMIZE_FILENAME}" > kustomize.SHA256
 sha256sum -c kustomize.SHA256
