6c88dd679d89035eaef1202726ff162c8ef90519
Brandon High <highb@users.noreply.github.com>
2023-02-27 20:33:27 -0800

    Move script out for better analysis

---
+++ download-and-verify.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p /tmp/bin
+pushd /tmp/bin || exit 1
+
+if [ -f "${KUSTOMIZE_FILENAME}" ]; then
+    echo "Kustomize already downloaded"
+else
+    curl -o "${KUSTOMIZE_FILENAME}" -L "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${VERSION}/${KUSTOMIZE_FILENAME}"
+fi
+sha256sum "${KUSTOMIZE_FILENAME}"
+echo "${SHA256_CHECKSUM}  ${KUSTOMIZE_FILENAME}" > kustomize.SHA256
+sha256sum -c kustomize.SHA256
+tar xzf "${KUSTOMIZE_FILENAME}"
+chmod u+x kustomize
+popd
+
+echo "/tmp/bin" >> $GITHUB_PATH
+/tmp/bin/kustomize version
+kustomize version
